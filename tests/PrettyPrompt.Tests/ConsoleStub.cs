719bf325a2c8b9309755a1352aa0d3d6eae15f49
Hubert Kindermann <kindermannhubert@users.noreply.github.com>
2021-12-08 20:41:46 +0100

    Possibility to invoke actions after key strokes in tests.

--- tests/PrettyPrompt.Tests/ConsoleStub.cs
+++ tests/PrettyPrompt.Tests/ConsoleStub.cs
@@ -4,20 +4,19 @@
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 #endregion
 
-using NSubstitute;
-using NSubstitute.Core;
-using PrettyPrompt.Consoles;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
-using static System.ConsoleModifiers;
+using NSubstitute;
+using NSubstitute.Core;
+using PrettyPrompt.Consoles;
 
 namespace PrettyPrompt.Tests
 {
     public static class ConsoleStub
     {
-        private static readonly Regex FormatStringSplit = new Regex(@"({\d+}|.)");
+        private static readonly Regex FormatStringSplit = new(@"({\d+}|.)", RegexOptions.Compiled);
 
         public static IConsole NewConsole(int width = 100, int height = 100)
         {
@@ -40,18 +39,57 @@ namespace PrettyPrompt.Tests
 
         /// <summary>
         /// Stub Console.ReadKey to return a series of keystrokes (<see cref="ConsoleKeyInfo" />).
-        /// Keystrokes are specified as a <see cref="System.FormattableString"/> with any special keys,
+        /// Keystrokes are specified as a <see cref="FormattableString"/> with any special keys,
         /// like modifiers or navigation keys, represented as FormattableString arguments (of type
         /// <see cref="ConsoleModifiers"/> or <see cref="ConsoleKey"/>).
         /// </summary>
         /// <example>$"{Control}LHello{Enter}" is turned into Ctrl-L, H, e, l, l, o, Enter key</example>
         public static ConfiguredCall StubInput(this IConsole consoleStub, params FormattableString[] inputs)
         {
-            List<ConsoleKeyInfo> keys = inputs
+            var keys = inputs
                 .SelectMany(line => MapToConsoleKeyPresses(line))
                 .ToList();
 
             return consoleStub.StubInput(keys);
+        }
+
+        /// <summary>
+        /// Stub Console.ReadKey to return a series of keystrokes (<see cref="ConsoleKeyInfo" />).
+        /// Keystrokes are specified as a <see cref="FormattableString"/> with any special keys,
+        /// like modifiers or navigation keys, represented as FormattableString arguments (of type
+        /// <see cref="ConsoleModifiers"/> or <see cref="ConsoleKey"/>) and with optional Action to be invoked after key press.
+        /// Use <see cref="Input(FormattableString)" and <see cref="Input(FormattableString, Action)"/> methods to create inputs./>
+        /// </summary>
+        public static ConfiguredCall StubInput(this IConsole consoleStub, params FormattableStringWithAction[] inputs)
+        {
+            var keys = inputs
+                .SelectMany(EnumerateKeys)
+                .ToList();
+
+            return consoleStub
+                .ReadKey(intercept: true)
+                .Returns(keys.First(), keys.Skip(1).ToArray());
+
+            IEnumerable<Func<CallInfo, ConsoleKeyInfo>> EnumerateKeys(FormattableStringWithAction input)
+            {
+                var keyPresses = MapToConsoleKeyPresses(input.Input);
+                if (keyPresses.Count > 0)
+                {
+                    for (int i = 0; i < keyPresses.Count - 1; i++)
+                    {
+                        yield return _ => keyPresses[i];
+                    }
+                    yield return _ =>
+                    {
+                        input.ActionAfter?.Invoke();
+                        return keyPresses[^1];
+                    };
+                }
+                else if (input.ActionAfter != null)
+                {
+                    throw new InvalidOperationException("you can specify 'actionAfter' only after keyPress");
+                }
+            }
         }
 
         public static ConfiguredCall StubInput(this IConsole consoleStub, List<ConsoleKeyInfo> keys)
@@ -139,5 +177,23 @@ namespace PrettyPrompt.Tests
                 ConsoleKey.Spacebar => ' ',
                 _ => '\0' // home, enter, arrow keys, etc
             };
+
+        public static FormattableStringWithAction Input(FormattableString input) => new(input);
+        public static FormattableStringWithAction Input(FormattableString input, Action actionAfter) => new(input, actionAfter);
+
+        public readonly struct FormattableStringWithAction
+        {
+            public readonly FormattableString Input;
+            public readonly Action ActionAfter;
+
+            public FormattableStringWithAction(FormattableString input)
+                : this(input, null) { }
+
+            public FormattableStringWithAction(FormattableString input, Action actionAfter)
+            {
+                Input = input;
+                ActionAfter = actionAfter;
+            }
+        }
     }
 }
