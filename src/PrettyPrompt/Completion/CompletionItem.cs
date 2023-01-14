9fd088a96cff6e8c12843c079476b5768207d33e
Hubert Kindermann <kindermannhubert@users.noreply.github.com>
2023-01-14 16:20:40 +0100

    Improved GetCompletionItemPriority with LevenshteinDistance and case sensitiveness.

--- src/PrettyPrompt/Completion/CompletionItem.cs
+++ src/PrettyPrompt/Completion/CompletionItem.cs
@@ -87,14 +87,15 @@ public class CompletionItem
     /// Integer representing priority of the item in completion list. Negative priorities represents
     /// non-matching items.
     /// </returns>
-    public virtual int GetCompletionItemPriority(string text, int caret, TextSpan spanToBeReplaced)
+    public virtual double GetCompletionItemPriority(string text, int caret, TextSpan spanToBeReplaced)
     {
         if (spanToBeReplaced.IsEmpty)
         {
             return 0;
         }
 
-        const int PriorityChunk = 1000;
+        const double MaxPriority = 10_000_000f;
+        const double PriorityChunk = 1_000_000f;
         var pattern = text.AsSpan(spanToBeReplaced);
 
         ReadOnlySpan<char> valueLonger;
@@ -110,29 +111,82 @@ public class CompletionItem
             valueShorter = FilterText;
         }
 
-        int distance;
+        double distance;
         if (valueLonger.StartsWith(valueShorter, StringComparison.CurrentCultureIgnoreCase))
         {
-            distance = Math.Abs(valueShorter.Length - valueLonger.Length);
+            distance = LevenshteinDistance(valueShorter, valueLonger);
         }
         else if (valueLonger.Contains(valueShorter, StringComparison.CurrentCultureIgnoreCase))
         {
-            distance = PriorityChunk + Math.Abs(valueShorter.Length - valueLonger.Length);
+            distance = PriorityChunk + LevenshteinDistance(valueShorter, valueLonger);
         }
         else
         {
-            return int.MinValue; //completely non-matching item
+            return double.MinValue; //completely non-matching item
         }
 
         if (pattern.Length <= FilterText.Length)
         {
             //matching item
-            return int.MaxValue - distance;
+            return MaxPriority - distance;
         }
         else
         {
             //non-matching item, but it's contained in pattern (which is better than completely unmatching)
             return -distance;
         }
+    }
+
+    internal static double LevenshteinDistance(ReadOnlySpan<char> s, ReadOnlySpan<char> t, double letterCaseChange= 0.001)
+    {
+        //https://en.wikipedia.org/wiki/Levenshtein_distance
+
+        // create two work vectors of integer distances
+        Span<double> v0 = stackalloc double[t.Length + 1];
+        Span<double> v1 = stackalloc double[t.Length + 1];
+
+        // initialize v0 (the previous row of distances)
+        // this row is A[0][i]: edit distance from an empty s to t;
+        // that distance is the number of characters to append to s to make t.
+        for (int i = 0; i <= t.Length; i++)
+        {
+            v0[i] = i;
+        }
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            // calculate v1 (current row distances) from the previous row v0
+
+            // first element of v1 is A[i + 1][0]
+            //   edit distance is delete (i + 1) chars from s to match empty t
+            v1[0] = i + 1;
+
+            // use formula to fill in the rest of the row
+            for (int j = 0; j < t.Length; j++)
+            {
+                // calculating costs for A[i + 1][j + 1]
+                var deletionCost = v0[j + 1] + 1;
+                var insertionCost = v1[j] + 1;
+
+                double substitutionCost = v0[j];
+                if (s[i] != t[j])
+                {
+                    substitutionCost +=
+                        char.ToUpperInvariant(s[i]) == char.ToUpperInvariant(t[j]) ?
+                        letterCaseChange :
+                        1d;
+                }
+
+                v1[j + 1] = Math.Min(Math.Min(deletionCost, insertionCost), substitutionCost);
+            }
+
+            // copy v1 (current row) to v0 (previous row) for next iteration
+            // since data in v1 is always invalidated, a swap without copy could be more efficient
+            var vTmp = v0;
+            v0 = v1;
+            v1 = vTmp;
+        }
+        // after the last swap, the results of v1 are now in v0
+        return v0[t.Length];
     }
 }
